d7d49f6bc910169c10216e08bc837b02e5ecbce5
Nathan-jiang <53858898+nathan-jhh@users.noreply.github.com>
2023-01-07 18:23:16 +0800

    主界面布局搭建2

--- App/GoodTalk/app/src/main/java/net/nathan/goodtalk/push/MainActivity.java
+++ App/GoodTalk/app/src/main/java/net/nathan/goodtalk/push/MainActivity.java
@@ -1,16 +1,74 @@
 package net.nathan.goodtalk.push;
 
 
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import net.nathan.goodtalk.common.app.Activity;
+import net.nathan.goodtalk.common.widget.PortraitView;
+import net.qiujuer.genius.ui.widget.FloatActionButton;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+    @BindView(R.id.appbar)
+    View mLayAppbar;
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
+
+    @BindView(R.id.btn_action)
+    FloatActionButton mAction;
+
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
 
     @Override
+    protected void initWidget() {
+        super.initWidget();
+
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mLayAppbar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
+    }
+
+    @Override
     protected void initData() {
         super.initData();
+    }
+
+    @OnClick({R.id.im_search})
+    void onSearchMenuClick() {
+
+    }
+
+    @OnClick(R.id.btn_action)
+    void onActionClick() {
+
     }
 }
