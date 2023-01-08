5d08a0da672fa662052e761d4657a053eb0d4b02
Nathan-jiang <53858898+nathan-jhh@users.noreply.github.com>
2023-01-08 20:09:11 +0800

    主界面底部导航栏封装完成
    bug：第一次打开app浮动按钮还是显示出来了

--- App/GoodTalk/app/src/main/java/net/nathan/goodtalk/push/MainActivity.java
+++ App/GoodTalk/app/src/main/java/net/nathan/goodtalk/push/MainActivity.java
@@ -3,8 +3,10 @@ package net.nathan.goodtalk.push;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -15,15 +17,21 @@ import com.bumptech.glide.request.target.ViewTarget;
 
 import net.nathan.goodtalk.common.app.Activity;
 import net.nathan.goodtalk.common.widget.PortraitView;
+import net.nathan.goodtalk.push.frags.main.ActiveFragment;
+import net.nathan.goodtalk.push.frags.main.ContactFragment;
+import net.nathan.goodtalk.push.frags.main.GroupFragment;
 import net.nathan.goodtalk.push.helper.NavHelper;
+import net.qiujuer.genius.ui.Ui;
 import net.qiujuer.genius.ui.widget.FloatActionButton;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity
         implements BottomNavigationView.OnNavigationItemSelectedListener,
-        NavHelper.OnTabChangedListener<Integer>{
+        NavHelper.OnTabChangedListener<Integer> {
     @BindView(R.id.appbar)
     View mLayAppbar;
 
@@ -42,7 +50,7 @@ public class MainActivity extends Activity
     @BindView(R.id.btn_action)
     FloatActionButton mAction;
 
-    private NavHelper mNavHelper;
+    private NavHelper<Integer> mNavHelper;
 
     @Override
     protected int getContentLayoutId() {
@@ -55,6 +63,10 @@ public class MainActivity extends Activity
 
         mNavHelper = new NavHelper<>(this, R.id.lay_container,
                 getSupportFragmentManager(), this);
+
+        mNavHelper.add(R.id.action_home, new NavHelper.Tab<>(ActiveFragment.class, R.string.title_home))
+                .add(R.id.action_group, new NavHelper.Tab<>(GroupFragment.class, R.string.title_group))
+                .add(R.id.action_contact, new NavHelper.Tab<>(ContactFragment.class, R.string.title_contact));
 
         // 底部导航栏设置监听
         mNavigation.setOnNavigationItemSelectedListener(this);
@@ -73,6 +85,11 @@ public class MainActivity extends Activity
     @Override
     protected void initData() {
         super.initData();
+
+        Menu menu = mNavigation.getMenu();
+        // 第一次进入app，手动选中home页
+        menu.performIdentifierAction(R.id.action_home, 0);
+
     }
 
     @OnClick({R.id.im_search})
@@ -86,7 +103,6 @@ public class MainActivity extends Activity
     }
 
     /**
-     *
      * @param item
      * @return true代表能处理点击，然后才会切换导航栏
      */
@@ -97,6 +113,35 @@ public class MainActivity extends Activity
 
     @Override
     public void onTabChanged(NavHelper.Tab<Integer> newTab, NavHelper.Tab<Integer> oldTab) {
-        
+        // 从额外字段中取出我们的Title资源Id
+        mTitle.setText(newTab.extra);
+
+        // 对浮动按钮进行隐藏与显示的动画
+        float transY = 0;
+        float rotation = 0;
+        if (Objects.equals(newTab.extra, R.string.title_home)) {
+            // 主界面时隐藏
+            transY = Ui.dipToPx(getResources(), 76);
+        } else {
+            // transY 默认为0 则显示
+            if (Objects.equals(newTab.extra, R.string.title_group)) {
+                // 群
+                mAction.setImageResource(R.drawable.ic_group_add);
+                rotation = -360;
+            } else {
+                // 联系人
+                mAction.setImageResource(R.drawable.ic_contact_add);
+                rotation = 360;
+            }
+        }
+
+        // 开始动画
+        // 旋转，Y轴位移，弹性差值器，时间
+        mAction.animate()
+                .rotation(rotation)
+                .translationY(transY)
+                .setInterpolator(new AnticipateOvershootInterpolator(1))
+                .setDuration(480)
+                .start();
     }
 }
